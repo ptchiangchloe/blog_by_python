59958510c62280ffc8f3d97088fb81f8f49edadd
Han Jiang <ptchiang12@gmail.com>
2017-06-21 16:09:36 -0700

    Upload image files.

--- src/posts/views.py
+++ src/posts/views.py
@@ -8,10 +8,12 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
 from .models import Post
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 # Create your views here.
 
 def post_create(request):
-    form = PostForm(request.POST or None)
+    form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
         print form.cleaned_data.get("title")
@@ -39,7 +41,18 @@ def post_detail(request, id=None):
     return render(request, 'post_detail.html', context)
 
 def post_list(request):
-    queryset = Post.objects.all()
+    queryset_list = Post.objects.all().order_by("-timestamp")
+    paginator = Paginator(queryset_list, 5) # Show 25 contacts per page
+
+    page = request.GET.get('abc')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
     if request.user.is_authenticated():
         context = {
             "object_list": queryset,
@@ -53,7 +66,7 @@ def post_list(request):
 
 def post_update(request, id=None):
     instance = get_object_or_404(Post, id=id)
-    form = PostForm(request.POST or None, instance=instance)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         print form.cleaned_data.get("title")
