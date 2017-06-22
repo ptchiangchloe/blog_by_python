9df92a1517e0cd50a9042bc92608ec4e78341de3
Han Jiang <ptchiang12@gmail.com>
2017-06-22 13:43:03 -0700

    Add social links.

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models
 from django.core.urlresolvers import reverse
+from django.db import models
+from django.db.models.signals import pre_save
+
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -10,9 +13,10 @@ def upload_location(instance, filename):
     # filebase, extension = filename.split(".")
     # return "%s/%s.%s" %(instance.id, instance.id, filename)
     return "%s/%s" %(instance.id, filename)
-    
+
 class Post(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to=upload_location,
             null=True, blank=True,
             width_field="width_field",
@@ -32,3 +36,20 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse("detail", kwargs={"id": self.id})
         # return "/posts/%s/" %(self.id)
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Post.objects.filter(slug=slug).order_by("-id")
+    exists = qs.exists()
+    if exists:
+        new_slug = "%s-%s" %(slug, qs.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from urllib import quote_plus
 from django.contrib import messages
 from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
@@ -34,9 +34,11 @@ def post_create(request):
 def post_detail(request, id=None):
     # instance = Post.objects.get(id=1)
     instance = get_object_or_404(Post, id=id)
+    share_string = quote_plus(instance.content)
     context = {
         "title": instance.title,
-        "instance": instance
+        "instance": instance,
+        "share_string": share_string,
     }
     return render(request, 'post_detail.html', context)
 
